dbe1e7661e4a3c8fee1d1603d1f87edc8a54aa41
Marwa Mazini <marwa6mzi@gmail.com>
2026-08-05 15:27:00 +0100

    Fix GitHub Pages paths

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/outati-electro-service/",
+
   server: {
     host: "0.0.0.0",
     port: 5173,
   },
+
   preview: {
     host: "0.0.0.0",
     port: 4173,
   },
+
   build: {
     outDir: "dist",
     emptyOutDir: true,
